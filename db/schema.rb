01e46e7fa0a94a1e3edd59e20d03c797a82897ed
vsspnkkvr <112337917+vsspnkkvr@users.noreply.github.com>
2023-04-29 18:55:21 -0400

    comments with assoiciated tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_21_193532) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_29_203436) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "pages", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -26,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_21_193532) do
     t.text "body"
   end
 
+  add_foreign_key "comments", "posts"
 end
